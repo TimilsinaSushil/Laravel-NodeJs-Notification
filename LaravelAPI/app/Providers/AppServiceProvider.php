96f3b05d4e89d8211be867fc22b1d8c2c4d8058c
Sushil Timilsina <developer.timilsina@gmail.com>
2025-06-03 12:08:13 +0545

    rate limiting applied

--- LaravelAPI/app/Providers/AppServiceProvider.php
+++ LaravelAPI/app/Providers/AppServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Cache\RateLimiting\Limit;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +22,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // allows no more than 10 requests per user per hour
+       RateLimiter::for('notifications', function (Request $request) {
+        return Limit::perHour(10)->by(optional($request->user())->id ?: $request->ip());
+    });
     }
 }
